3121bd30ee4215752164787d4339968a84a6d8fc
未知 <1002409455@qq.com>
2016-08-09 13:29:43 +0800

    Initial Commit

---
+++ UDisk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UDisk
+//
+//  Created by shiki on 16/8/9.
+//  Copyright © 2016年 两仪式. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UDisk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UDisk
+//
+//  Created by shiki on 16/8/9.
+//  Copyright © 2016年 两仪式. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
